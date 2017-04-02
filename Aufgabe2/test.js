b6d962d8c4f271535a74f1433b2c75d8ce736234
Kiara <Kiara@kiaras-air.fritz.box>
2017-04-02 17:11:13 +0200

    Aufgabe 2 - Final Version

--- Aufgabe2/test.js
+++ Aufgabe2/test.js
@@ -1,3 +1,13 @@
+/*
+Aufgabe: Aufgabe 2a: Dynamic HTML
+Name: Kiara Luana Oßwald
+Matrikel: 254140
+Datum: 02.04.2017
+    
+Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde nicht kopiert und auch nicht diktiert.
+
+Dieser Code wurde zusammen mit Alfred Müller und mit Hilfestellung von Moritz Giuliani erstellt.
+*/
 document.addEventListener("DOMContentLoaded", function () {
     console.log("test");
     let board = document.createElement("section");
@@ -5,6 +15,8 @@ document.addEventListener("DOMContentLoaded", function () {
     for (var i = 0; i < 64; i++) {
         let field = document.createElement("div");
         board.appendChild(field);
+        let rice = Math.pow(2, i);
+        field.innerText = "" + rice;
         console.log(i);
     }
 });
--- Aufgabe2/test.ts
+++ Aufgabe2/test.ts
@@ -1,3 +1,16 @@
+/*
+Aufgabe: Aufgabe 2a: Dynamic HTML
+Name: Kiara Luana Oßwald
+Matrikel: 254140
+Datum: 02.04.2017
+    
+Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde nicht kopiert und auch nicht diktiert.
+
+Dieser Code wurde zusammen mit Alfred Müller und mit Hilfestellung von Moritz Giuliani erstellt.
+*/
+
+
+
 document.addEventListener("DOMContentLoaded", function(): void {
     console.log("test");
     
@@ -8,9 +21,13 @@ document.addEventListener("DOMContentLoaded", function(): void {
     for (var i: number = 0; i < 64; i++) {
         let field: HTMLDivElement = document.createElement("div");
         board.appendChild(field);
+        let rice: number = Math.pow(2, i);
+        field.innerText = "" + rice;
         
         console.log(i);
         
         }
+    
+    
     
     });
