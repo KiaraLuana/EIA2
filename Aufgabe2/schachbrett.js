1885ef4e87e9a8de4d3993ad35426bb88a69d585
Kiara <Kiara@kiaras-air.fritz.box>
2017-04-02 14:41:31 +0200

    Aufgabe 2

---
+++ Aufgabe2/schachbrett.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var n = 64;
+    var rice = 1;
+    for (let n = 0; n < 64; n++)
+        ;
+});
+//# sourceMappingURL=schachbrett.js.map
---
+++ Aufgabe2/schachbrett.ts
@@ -0,0 +1,35 @@
+document.addEventListener("DOMContentLoaded", function() {
+    
+    var n: number = 64;
+    var rice: number = 1;
+    
+    for (let i = 0; n < 64; n++); {
+        let field: HTMLDivELement = document.createElement("div");
+        let row: number = 1;
+       
+        if (row % 2 != 0) {
+            if (i % 2 == 0) {
+            color = "#000000";
+            }
+        
+        else {
+            color = "#FFFFFF";
+    }
+            }
+        else {
+            if (i % 2 == 0) {
+                color = "#FFFFFF";
+                }
+            else {
+                color = "#000000";
+                }
+            if (i % 8 == 0) {
+            row++;
+            }
+            rice: number == Math.pow(2, i);
+            document.getElementsByTagName("DIV").textContent[i] = "" + rice;
+            
+            }
+      }  
+        }
+    });
